3863ac98968d05c24d7dce0cf32dd0e9a02ee660
Natorden <71587437+Natorden@users.noreply.github.com>
2023-04-14 12:07:52 +0200

    added ocelot api gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot-aggregation.json");
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+//app.UseHttpsRedirection();
+
+//app.UseHttpMetrics();
+
+//app.MapMetrics(); //Doesn't work here for some unknown reason.
+// Do it the old way instead:
+app.UseRouting();
+app.UseAuthorization();
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapMetrics();
+// });
+
+app.UseOcelot().Wait();
+
+app.Run();
