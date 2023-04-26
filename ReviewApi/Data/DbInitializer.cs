98f7608b74b7f0f6a0b5de0de55d6d917409c3d8
Tegridy Code / Asyno <47446626+Asynoo@users.noreply.github.com>
2023-04-26 16:43:10 +0200

    Seeding Data

--- ReviewApi/Data/DbInitializer.cs
+++ ReviewApi/Data/DbInitializer.cs
@@ -1,25 +1,26 @@
-﻿using ReviewApi.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewApi.Models;
+
 namespace ReviewApi.Data {
     public class DbInitializer : IDbInitializer {
         // This method will create and seed the database.
-        public void Initialize(ReviewApiContext context) {
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+        public async void Initialize(ReviewApiContext context) {
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
 
-            // Look for any Products
-            if (context.Reviews.Any()) {
+            // Look for any Reviews
+            if (await context.Reviews.AnyAsync()) {
                 return; // DB has been seeded
             }
 
             List<Review> reviews = new List<Review> {
-                //TODO: Seed some reviews
-                //new() { Name = "Hammer", Price = 100, ItemsInStock = 10, ItemsReserved = 3 },
-                //new() { Name = "Screwdriver", Price = 70, ItemsInStock = 20, ItemsReserved = 2 },
-                //new() { Name = "Drill", Price = 500, ItemsInStock = 20, ItemsReserved = 0 }
+                new() { ProductId = 1, CustomerId = 1, Title = "Great product!", Rating = 0.9, Description = "I love this product. It does exactly what it's supposed to do." },
+                new() { ProductId = 2, CustomerId = 2, Title = "Disappointed", Rating = 0.4, Description = "I had high hopes for this product, but it didn't live up to my expectations." },
+                new() { ProductId = 3, CustomerId = 3, Title = "Average", Rating = 0.6, Description = "This product was okay. Nothing special, but it got the job done." }
             };
 
-            context.Reviews.AddRange(reviews);
-            context.SaveChanges();
+            await context.Reviews.AddRangeAsync(reviews);
+            await context.SaveChangesAsync();
         }
     }
 }
