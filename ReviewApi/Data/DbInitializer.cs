f393b4c67be133602f01535dd59749cbf1d837c8
Tegridy Code / Asyno <47446626+Asynoo@users.noreply.github.com>
2023-04-26 16:49:11 +0200

    A little more seeding

--- ReviewApi/Data/DbInitializer.cs
+++ ReviewApi/Data/DbInitializer.cs
@@ -16,7 +16,12 @@ namespace ReviewApi.Data {
             List<Review> reviews = new List<Review> {
                 new() { ProductId = 1, CustomerId = 1, Title = "Great product!", Rating = 0.9, Description = "I love this product. It does exactly what it's supposed to do." },
                 new() { ProductId = 2, CustomerId = 2, Title = "Disappointed", Rating = 0.4, Description = "I had high hopes for this product, but it didn't live up to my expectations." },
-                new() { ProductId = 3, CustomerId = 3, Title = "Average", Rating = 0.6, Description = "This product was okay. Nothing special, but it got the job done." }
+                new() { ProductId = 3, CustomerId = 3, Title = "Average", Rating = 0.6, Description = "This product was okay. Nothing special, but it got the job done." },
+                new() { ProductId = 4, CustomerId = 4, Title = "Waste of money", Rating = 0.2, Description = "I regret buying this product. It didn't work at all and was a complete waste of money." },
+                new() { ProductId = 5, CustomerId = 5, Title = "Excellent product", Rating = 0.9, Description = "This product exceeded my expectations. It's well-made and works perfectly." },
+                new() { ProductId = 6, CustomerId = 6, Title = "Not worth it", Rating = 0.3, Description = "I wouldn't recommend this product. It's not worth the price and didn't work as well as I had hoped." },
+                new() { ProductId = 7, CustomerId = 7, Title = "Decent", Rating = 0.6, Description = "This product was decent. It did what it was supposed to, but I wasn't blown away by it." },
+                new() { ProductId = 8, CustomerId = 8, Title = "Terrible", Rating = 0.1, Description = "I had a terrible experience with this product. It broke within a week and the customer service was unhelpful." }
             };
 
             await context.Reviews.AddRangeAsync(reviews);
